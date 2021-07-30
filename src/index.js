c8039b94e39a074bb60a4157f50cbc6e7ed8b685
jkmor31 <jamesmorical@gmail.com>
2021-07-30 12:00:26 -0500

    Remove index.css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { configureStore } from './store';
-import './index.css';
 import App from './App';
 
 const store = configureStore();
