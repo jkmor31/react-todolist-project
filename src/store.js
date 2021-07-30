d6b2592bd9fa078c5608e1b0136aa9b91f96bb49
jkmor31 <jamesmorical@gmail.com>
2021-07-30 11:54:01 -0500

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStoreHook, combineReducers, createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
